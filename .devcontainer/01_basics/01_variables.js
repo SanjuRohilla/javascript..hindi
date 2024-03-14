6bfd59ce63b6f542530b39e914d9d5298ddfa8e0
SanjuRohilla <155614568+SanjuRohilla@users.noreply.github.com>
2024-03-14 18:15:24 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "sanju@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState 
+// accountId = 2 //not allowed
+
+accountEmail = "rahul@2024.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.log("accountId")
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+prefer not to use var coz of issue in block scope and functional scope
+*/
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all code as newer version of js, agr ye na bhi likhe to bhi automatic new version hi 
+
+//alert(3+3)       //we are using node.js , not browser
+
+//code readibility honi chaiye
+
+let name = "hitesh"  //string , single col '' doubecol " " dono use kar skte hai
+let age = 18 //number , range of 2 to power of 53
+//number => 2 to power 53
+// bigint
+// string => " " , ' '
+// boolean => true/false
+// null => standlone value ( empty space)
+// undefined => variable declare kiya hai but value assign nhi ki
+// symbol => for uniqueness
+
+
+//object
+
+console.log(typeof "hello")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
