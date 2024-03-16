3bad35ed7ed9e7b1655c295b8bbff471587f3faf
SanjuRohilla <155614568+SanjuRohilla@users.noreply.github.com>
2024-03-16 17:00:58 +0000

    string to number conversion

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,60 @@
+let score = false
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+// "33" => 33
+// "33asc" => NaN (not a number)
+// true => 1
+// false => 0
+// null => 0
+
+let isLoggedIn = 1
+
+let someNumber = 33
+let stringNumber= String(someNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+let a = (5-5)
+let b = Boolean(a)
+//console.log(typeof b)
+//console.log(b)
+
+//*****************************operations**************************
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*3);
+console.log(2**3); //two ki power 3
+console.log(1/3);
+console.log(6%2);
+
+let str1 = "hello"
+let str2 = "  hitesh"
+
+let str3 = str1+str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log(12+"hello")
+console.log("1" +2+2)
+console.log(1+2+"2")
+console.log(3+4*5%3) //aisa kaam na hi kre
+console.log(true)
+console.log(+true)
+console.log(false)
+console.log(+false)
+console.log(+"")
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
